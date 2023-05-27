639a3cc69c1464c0d24f8336123a3950d0b5e074
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-27 21:05:16 +0430

    The Product and Stock classes were created

---
+++ HW5/Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿
+
+using IronBarCode;
+
+namespace HW5.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public BarcodeResult BarCode { get; set; }
+
+        public string Name { get; set; }
+
+
+
+    }
+}
---
+++ HW5/Domain/Stock.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HW5.Domain
+{
+    public class Stock
+    {
+        public Guid StockId { get; set; }
+        public string Name { get; set; }
+        public Guid ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal ProductPrice { get; set; }
+
+    }
+}
