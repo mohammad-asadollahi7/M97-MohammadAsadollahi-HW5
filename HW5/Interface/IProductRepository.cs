7cafc32a5e94eb3cdcf25cac5c92ab783bda5003
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-27 21:21:07 +0430

    The IProductRepository was created

---
+++ HW5/Interface/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using HW5.Domain;
+
+namespace HW5.Interface
+{
+    public interface IProductRepository
+    {
+        string AddProduct(Product product);
+        List<Product> GetProductList();
+        string GetProductById(int id);
+    }
+}
