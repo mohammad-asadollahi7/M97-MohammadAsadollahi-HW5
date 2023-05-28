2b8c80a4ff4f402c6a4b8064af5268e278ef8d3c
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-28 14:00:55 +0430

    The constructure of ProductRepository class and SetData and AddProduct method were added.

--- HW5/Interface/ProductRepository.cs
+++ HW5/Interface/ProductRepository.cs
@@ -1,13 +1,39 @@
 ﻿using HW5.Domain;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
 namespace HW5.Interface
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly List<Product>? _products;
+        private string? jsonFilePath;
+        private string? jsonString;
+        public ProductRepository()
+        {
+            string? projectPath = Directory.GetParent
+                   (AppDomain.CurrentDomain.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+            jsonFilePath = Path.Combine(projectPath, "DataBase/Product.json");
+            jsonString = File.ReadAllText(jsonFilePath);
+            _products = JsonConvert.DeserializeObject<List<Product>>(jsonString);
+        }
+
         public string AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            
+            bool isValid = CheckProductName(product.Name);
+            if (isValid)
+            {
+                _products.Add(product);
+                SetData(_products);
+                return "The product was added successfully.";
+            }
+            else
+            {
+                return "The selected product name is not correct.";
+            }
         }
 
         public string GetProductById(int id)
@@ -23,6 +49,12 @@ namespace HW5.Interface
         public bool CheckProductName(string productName)
         {
             return Regex.IsMatch(productName, "^[A-Z]{1}[a-z]{3}[a-zA-Z0-9]{1}_{1}[0-9]{3}$");
+        }
+
+        public void SetData(List<Product> products)
+        {
+            jsonString = JsonConvert.SerializeObject(products);
+            File.WriteAllText(jsonFilePath, jsonString);
         }
     }
 }
