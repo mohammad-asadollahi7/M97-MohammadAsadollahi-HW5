ed7a7bed573f21591516437287112804642b4d89
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-28 12:53:31 +0430

    The checkProductName method was written

--- HW5/Interface/ProductRepository.cs
+++ HW5/Interface/ProductRepository.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HW5.Domain;
+using System.Text.RegularExpressions;
 
 namespace HW5.Interface
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        public string AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckProductName(string productName)
+        {
+            return Regex.IsMatch(productName, "^[A-Z]{1}[a-z]{3}[a-zA-Z0-9]{1}_{1}[0-9]{3}$");
+        }
     }
 }
