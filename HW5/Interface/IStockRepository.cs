83d69138e6a7335541de89a352e7029442028c2d
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-28 17:26:38 +0430

    The StockRepo class and interface were creatd

---
+++ HW5/Interface/IStockRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using HW5.Domain;
+
+namespace HW5.Interface
+{
+    public interface IStockRepository
+    {
+        string SaleProduct(int ProductId, int cnt);
+        string BuyProduct(Stock productInStock);
+        List<StockProductViewModel> GetSalesProductList();
+
+    }
+}
---
+++ HW5/Interface/StockRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using HW5.Domain;
+
+namespace HW5.Interface
+{
+    public class StockRepository : IStockRepository
+    {
+        public string BuyProduct(Stock productInStock)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public List<StockProductViewModel> GetSalesProductList()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public string SaleProduct(int ProductId, int cnt)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
