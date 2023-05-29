def8a6db2f2e066d68a74918696b00d545302f45
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-05-29 22:54:05 +0430

    add product ui was completed

--- HW5/UI/Program.cs
+++ HW5/UI/Program.cs
@@ -6,6 +6,32 @@ class Program
 {
     static void Main()
     {
-        
+        DBContext<Product> productDbContext = new DBContext<Product>();
+        DBContext<Stock> stockDbContext = new DBContext<Stock>();
+        IProductRepository productRepository = new ProductRepository(productDbContext);
+        IStockRepository stockRepository = new StockRepository(stockDbContext, productDbContext);
+
+        do
+        {
+            Console.WriteLine("1.Add product\n2.Viewing products list\n" +
+                             "3.Sale a product from stock" +
+                              "\n4.Buy a product for stock\n" +
+                              "5.Viewing sales prodcuts list\n6.Exit");
+            string menuOption = Console.ReadLine();
+
+            if (menuOption == "1")
+            {
+                Console.Clear();
+                Console.WriteLine("Enter the name(pattern: Azxcs_123):");
+                Product product = new Product();
+                product.Name = Console.ReadLine();
+                string message = productRepository.Add(product);
+                Console.Clear();
+                Console.WriteLine(message);
+                Console.ReadKey();
+            }
+
+
+        } while (true);
     }
 }
